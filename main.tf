1a0632dae46cd7906c71d27d88970be9f1395eb6
Alok Raja <94987623+alokraja075@users.noreply.github.com>
2025-12-22 18:28:54 +0000

    Add Terraform configuration files and provider setup

--- main.tf
+++ main.tf
@@ -1,26 +1,32 @@
 locals {
-    key_dir = "${path.root}/key"
+  key_dir = "${path.root}/key"
 }
 
 resource "null_resource" "SSHDir" {
-    triggers = {
-        key_dir=local.key_dir
-    }
-    provisioner "local-exec" {
-        command ="mkdir  ${self.triggers.key_dir}"
-    }
-    provisioner "local-exec" {
-        when = destroy
-        command ="rm -rf  ${self.triggers.key_dir}"
-    }
+  triggers = {
+    key_dir = local.key_dir
+  }
+
+  provisioner "local-exec" {
+    command = "mkdir -p ${self.triggers.key_dir}"
+  }
+
+  provisioner "local-exec" {
+    when    = destroy
+    command = "rm -rf ${self.triggers.key_dir}"
+  }
 }
+
 resource "null_resource" "SSHKey" {
-    provisioner "local-exec" {
-       command = <<-EOF
-        if [ ! -f "${local.key_dir}/${var.key_pair_name}" ]; then
-            ssh-keygen -t rsa  -b 4096 -f "$local.key_dir/$var.key_pair_name" -N ''
-            chmod 400 "$local.key_dir/$var.key_pair_name"
-        fi
-        EOF
-    }
+
+  depends_on = [null_resource.SSHDir]
+
+  provisioner "local-exec" {
+    command = <<-EOF
+      if [ ! -f "${local.key_dir}/${var.key_pair_name}" ]; then
+        ssh-keygen -t rsa -b 4096 -f "${local.key_dir}/${var.key_pair_name}" -N ""
+        chmod 400 "${local.key_dir}/${var.key_pair_name}"
+      fi
+    EOF
+  }
 }

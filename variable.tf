de264e350291185d1b771481a0ab0065f41517a7
Alok Raja <94987623+alokraja075@users.noreply.github.com>
2025-12-24 18:00:59 +0000

    Add security group and ingress rules variables to configuration

--- variable.tf
+++ variable.tf
@@ -2,3 +2,19 @@ variable "key_pair_name" {
   type    = string
   default = "ProdKP"
 }
+variable "security_group_name"{
+  type    = string
+  default = "Prod_SG"
+}
+variable "ingress_rules"{
+  type = map(object({
+    from_port = number
+    to_port = number
+    protocol = string
+    cidr_blocks = list(string)
+  }))
+}
+variable "instance_type"{
+  type = string
+  default   = "t2.micro"
+}
